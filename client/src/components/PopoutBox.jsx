9a9648c47f6921e4eeeda6bfe3152e1ab93c9997
caphilbr <caphilbr@hotmail.com>
2024-07-07 19:53:27 -0400

    UI for buttons, popout boxes, winner message

---
+++ client/src/components/PopoutBox.jsx
@@ -0,0 +1,10 @@
+const PopoutBox = () => {
+
+  return (
+    <div className="popout__container">
+      <div>Popout Box</div>
+    </div>
+  )
+}
+
+export default PopoutBox
---
+++ client/src/components/WinnerDisplay.jsx
@@ -0,0 +1,12 @@
+const WinnerDisplay = () => {
+  return (
+    <div className="winner__container">
+      <div className="winner__bounce winner__font">WINNER!</div>
+      <div className="button__container">
+        <span className="button__winner">Continue</span>
+      </div>
+    </div>
+  )
+}
+
+export default WinnerDisplay
